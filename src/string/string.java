39866c5c5455e84c077cdfd7dbbd861828b81fd2
imransalg <imransalgiriev5200@gmail.com>
2023-01-26 14:06:09 +0300

    fixed "string"

--- src/string/string.java
+++ src/string/string.java
@@ -9,24 +9,24 @@ public class string {
 
     public static void task1() {
         System.out.println("Задание 1");
-        String firstName = " Ivan ";
-        String middleName = "Ivanov ";
-        String lastName = " Ivanovich";
-        String fullName = middleName + firstName + lastName;
+        String firstName = "Ivan";
+        String middleName = "Ivanov";
+        String lastName = "Ivanovich";
+        String fullName = middleName + ' ' + firstName + ' ' + lastName;
         System.out.println("ФИО сотрудника - " + fullName);
     }
 
     public static void task2() {
         System.out.println("Задание 2");
-        String firstName = " Ivan ";
-        String middleName = "Ivanov ";
-        String lastName = " Ivanovich";
-        String fullName = middleName + firstName + lastName;
+        String firstName = "Ivan";
+        String middleName = "Ivanov";
+        String lastName = "Ivanovich";
+        String fullName = middleName + ' ' + firstName + ' ' + lastName;
         System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fullName.toUpperCase());
     }
     public static void task3() {
         System.out.println("Задание 3");
-        String fullName = " Иванов Семён Семёнович ";
+        String fullName = "Иванов Семён Семёнович";
         fullName = fullName.replace("ё", "е");
         System.out.println("ФИО сотрудника - " + fullName);
     }
